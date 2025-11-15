a701bd5cb0ad2abea3af2466f98a28ac36d18184
Danjkah <daniel02_02@outlook.com>
2025-11-15 17:27:56 -0500

    configuations added

--- AssignmentThree.API/Program.cs
+++ AssignmentThree.API/Program.cs
@@ -1,8 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using AssignmentThree.Core.Interfaces;
+using AssignmentThree.Infrastructure.Data;
+using AssignmentThree.Infrastructure.Repositories;
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers().AddNewtonsoftJson(); // Required for JSON Patch support
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
+// Configure SQLite Database
+builder.Services.AddDbContext<AssignmentDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// Register Repository
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+
 
 var app = builder.Build();
 
@@ -12,28 +33,9 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+

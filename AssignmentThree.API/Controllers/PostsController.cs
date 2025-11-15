74675e7b53a58c6a0bb3cec99a9962a984102100
Danjkah <daniel02_02@outlook.com>
2025-11-15 14:41:43 -0500

    Post Controller created

---
+++ AssignmentThree.API/Controllers/PostsController.cs
@@ -0,0 +1,138 @@
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using AssignmentThree.Core.DTOs;
+using AssignmentThree.Core.Interfaces;
+using AssignmentThree.Core.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+namespace TaskManagement.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PostsController : ControllerBase
+{
+    private readonly IPostRepository _postRepository;
+
+ public PostsController(IPostRepository postRepository)
+    {
+        _postRepository = postRepository;
+    }
+
+    //GET: api/Post
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Post>>> GetAllPosts()
+    {
+        var posts = await _postRepository.GetAllPostsAsync();
+        return Ok(posts);
+    }
+
+    //GET: api/Posts/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Post>> GetPost(int id)
+    {
+        var post = await _postRepository.GetPostByIdAsync(id);
+
+        if (post == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(post);
+
+    }
+
+    //POST: api/Posts
+    [HttpPost]
+    public async Task<ActionResult<Task>> CreatePost([FromBody] PostCreateDto postDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState); //this is not in the requirements of the assignment but I added it in because it just makes sense to have
+        }
+
+        var post = new Post
+        {
+          Title = postDto.Title,
+          Content = postDto.Content,
+        
+        };
+
+        var createdPost = await _postRepository.CreatePostAsync(post);
+
+        return CreatedAtAction(
+            nameof(GetPost),
+            new {id = createdPost.Id},
+            createdPost
+        );
+    }
+
+    //PUT: api/tasks/{id}
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdatePost(int id, [FromBody] PostUpdateDto postDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);//this is not in the requirements of the assignment but I added it in because it just makes sense to have
+        }
+
+        var exists = await _postRepository.PostExistsAsync(id);
+        if (!exists)
+        {
+            return NotFound();
+        }
+
+        var post = new Post
+        {
+            Id = id,
+            Title = postDto.Title,
+            Content = postDto.Content,
+        };
+
+        var UpdatedPost = await _postRepository.UpdatePostAsync(post);
+
+        return Ok(UpdatedPost);
+    }
+    
+    //PATCH: api/posts/{id}
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> PatchPost(int id, [FromBody] JsonPatchDocument<Post> patchDoc)
+    {
+        if (patchDoc == null)
+        {
+            return BadRequest (new {message = "Patch document is null"});//this is not in the requirements of the assignment but I added it in because it just makes sense to have
+        }
+        var post = await _postRepository.GetPostByIdAsync(id);
+        if (post == null)
+        {
+            return NotFound();
+        }
+
+        patchDoc.ApplyTo(post, ModelState);
+
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);//this is not in the requirements of the assignment but I added it in because it just makes sense to have
+        }
+        post.UpdatedDate = DateTime.UtcNow;
+
+        await _postRepository.UpdatePostAsync(post);
+
+        return Ok(post);
+
+    }
+    
+    // DELETE: api/task/{id}
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePost(int id)
+    {
+        var deleted = await _postRepository.DeletePostAsync(id);
+        if (!deleted)
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+
+
+
+}
